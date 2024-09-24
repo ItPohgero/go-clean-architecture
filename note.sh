b4f194552e40613289aed9457f3ef4fcf69be6b4
Wahyu Agus Arifin <68760662+ItPohgero@users.noreply.github.com>
2024-09-24 15:54:57 +0700

    note

--- note.sh
+++ note.sh
@@ -5,3 +5,12 @@ go run cmd/web/main.go
 
 # Run Worker
 go run cmd/worker/main.go
+
+# New Migration
+migrate create -ext sql -dir db/migrations create_table_xxx
+
+# Run Migrations
+# Windows
+_/migrate/up.bat
+# Linux or MacOS
+_/migrate/up.sh
